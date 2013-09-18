c97c1b74fed9d4680c0abf5f7d34de73979541f0
jasand86 <joshs@2600hz.com>
2013-09-18 11:22:46 -0700

    edits

--- js/script.js
+++ js/script.js
@@ -112,8 +112,8 @@ $('#slideshow > div:gt(0)').hide();
 
 setInterval(function(){
     $('#slideshow > div:first')
-         .fadeOut(1000)
-         .next()
+        .fadeOut(1000)
+        .next()
         .fadeIn(1000)
         .end()
         .appendTo('#slideshow');
