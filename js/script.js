3cc9e395f53606e10c96319888cf722c3f93154a
jasand86 <joshs@2600hz.com>
2014-08-12 17:31:49 -0700

    2014 redesign v1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// -----------------------------------
+// HEXAGON BACKGROUND PARALLAX EFFECTS
+// -----------------------------------
+
+$(window).scroll(function(e){
+	parallax();
+});
+function parallax(){
+  	var scrolled = $(window).scrollTop();
+
+  	$('.hex-bg').css('top', -(scrolled * 1.6) + 'px');
+}
+
+// ----------------------------
+// COUNTDOWN UNTIL EVENT STARTS
+// ----------------------------
+
+$('#count_down').county({ 
+    endDateTime: new Date('2014/10/06 08:30:00'), 
+    reflection: false, 
+    animation: 'scroll' 
+    // theme: 'black' 
+});
+
+
+
+// $(window).scroll(function() {
+// 	// $('#animatedElement').each(function(){
+// 	var imagePos = $('.welcome').offset().top;
+// 	var topOfWindow = $(window).scrollTop();
+
+// 	if (imagePos < topOfWindow+700) {
+// 		$('body').addClass('bg-blue');
+// 	}
+// });
